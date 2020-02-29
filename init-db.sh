477c892035b161bfaa8d9f070ccf91655ae3ba5e
ggodreau <ggodreau@atlassian.com>
2020-02-29 17:22:45 -0600

    adds customers load to db

--- init-db.sh
+++ init-db.sh
@@ -2,3 +2,9 @@
 set -e
 
 tar -xzvf /var/lib/postgresql/rawdata/out_290220.gz -C /var/lib/postgresql/rawcsvs
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    COPY customers
+    FROM '/var/lib/postgresql/rawcsvs/customers.csv' DELIMITER ',' CSV HEADER;
+EOSQL
+
