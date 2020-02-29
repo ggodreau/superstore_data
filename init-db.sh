7e53e30d512394b1650555bc93dc04ba52562f90
ggodreau <ggodreau@atlassian.com>
2020-02-29 17:32:09 -0600

    adds create table for customers

--- init-db.sh
+++ init-db.sh
@@ -1,8 +1,20 @@
 #bin/bash
 set -e
 
+# extract data archive into csvs
 tar -xzvf /var/lib/postgresql/rawdata/out_290220.gz -C /var/lib/postgresql/rawcsvs
 
+# create customers table
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE TABLE persons
+    (
+      customer_id character varying(50),
+      customer_name character varying(50),
+      segment character varying(50)
+    );
+EOSQL
+
+# populate customers table
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     COPY customers
     FROM '/var/lib/postgresql/rawcsvs/customers.csv' DELIMITER ',' CSV HEADER;
