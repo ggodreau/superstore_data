e829cae372d98fca77ef72cd0b039a129b67d549
ggodreau <ggodreau@atlassian.com>
2020-02-29 17:42:28 -0600

    corrects customers table name

--- init-db.sh
+++ init-db.sh
@@ -7,7 +7,7 @@ tar -xzvf /var/lib/postgresql/rawdata/out_290220.gz -C /var/lib/postgresql/rawcs
 # create customers table
 echo Creating customers table...
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE TABLE persons
+    CREATE TABLE customers
     (
       customer_id character varying(50),
       customer_name character varying(50),
