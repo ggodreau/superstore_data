fcbea7ef0e31073a2a194a3aa674f9a795dac1f8
ggodreau <ggodreau@atlassian.com>
2020-03-01 14:16:45 -0600

    adds remaining tables to init script

--- init-db.sh
+++ init-db.sh
@@ -4,7 +4,10 @@ set -e
 # extract data archive (all csvs) into uncompressed csvs
 tar -xzvf /var/lib/postgresql/rawdata/out_290220.gz -C /var/lib/postgresql/rawcsvs
 
-# create customers table
+###########
+# CUSTOMERS
+###########
+
 echo Creating customers table...
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE TABLE customers
@@ -15,14 +18,16 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     );
 EOSQL
 
-# populate customers table
 echo Populating customers table...
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     COPY customers
     FROM '/var/lib/postgresql/rawcsvs/customers.csv' DELIMITER ',' CSV HEADER;
 EOSQL
 
-# create orders table
+########
+# ORDERS
+########
+
 echo Creating orders table...
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE TABLE orders
@@ -42,9 +47,76 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     );
 EOSQL
 
-# populate orders table
 echo Populating orders table...
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     COPY orders
     FROM '/var/lib/postgresql/rawcsvs/orders.csv' DELIMITER ',' CSV HEADER;
+EOSQL
+
+##########
+# PRODUCTS
+##########
+
+echo Creating products table...
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE TABLE products
+    (
+      product_id varchar(16) PRIMARY KEY,
+      category varchar(15),
+      sub_category varchar(11),
+      product_name varchar(109),
+      product_cost_to_consumer money
+    );
+EOSQL
+
+echo Populating products table...
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    COPY products
+    FROM '/var/lib/postgresql/rawcsvs/products.csv' DELIMITER ',' CSV HEADER;
+EOSQL
+
+#########
+# REGIONS
+#########
+
+echo Creating regions table...
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE TABLE regions
+    (
+      id integer PRIMARY KEY,
+      region varchar(14),
+      country varchar(32),
+      state varchar(36),
+      city varchar(35),
+      salesperson varchar(17),
+      postal_code varchar(20),
+      country_code varchar(20)
+    );
+EOSQL
+
+echo Populating regions table...
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    COPY regions
+    FROM '/var/lib/postgresql/rawcsvs/regions.csv' DELIMITER ',' CSV HEADER;
+EOSQL
+
+#########
+# RETURNS
+#########
+
+echo Creating returns table...
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE TABLE returns
+    (
+      order_id varchar(15) PRIMARY KEY,
+      return_date date,
+      return_quantity integer,
+      reason_returned varchar(11)
+    );
+EOSQL
+
+echo Populating returns table...
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    COPY regions
+    FROM '/var/lib/postgresql/rawcsvs/returns.csv' DELIMITER ',' CSV HEADER;
 EOSQL
