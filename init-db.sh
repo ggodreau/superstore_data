1680dbad4ef82cf4bd750f23f3535930bbba2ad8
ggodreau <ggodreau@atlassian.com>
2020-03-01 12:58:12 -0600

    - adds orders table - changes to primary key for customers

--- init-db.sh
+++ init-db.sh
@@ -1,7 +1,7 @@
 #bin/bash
 set -e
 
-# extract data archive into csvs
+# extract data archive (all csvs) into uncompressed csvs
 tar -xzvf /var/lib/postgresql/rawdata/out_290220.gz -C /var/lib/postgresql/rawcsvs
 
 # create customers table
@@ -9,9 +9,9 @@ echo Creating customers table...
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE TABLE customers
     (
-      customer_id character varying(50),
-      customer_name character varying(50),
-      segment character varying(50)
+      customer_id varchar(50) PRIMARY KEY,
+      customer_name varchar(50),
+      segment varchar(50)
     );
 EOSQL
 
@@ -22,3 +22,29 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     FROM '/var/lib/postgresql/rawcsvs/customers.csv' DELIMITER ',' CSV HEADER;
 EOSQL
 
+# create orders table
+echo Creating orders table...
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE TABLE orders
+    (
+      id integer PRIMARY KEY,
+      order_id varchar(20) NOT NULL UNIQUE,
+      order_date date,
+      ship_date date,
+      ship_mode varchar(20),
+      customer_id varchar(8) NOT NULL,
+      product_id varchar(16) NOT NULL,
+      sales money,
+      quantity integer,
+      discount real,
+      profit money,
+      region_id integer NOT NULL
+    );
+EOSQL
+
+# populate orders table
+echo Populating orders table...
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    COPY customers
+    FROM '/var/lib/postgresql/rawcsvs/orders.csv' DELIMITER ',' CSV HEADER;
+EOSQL
